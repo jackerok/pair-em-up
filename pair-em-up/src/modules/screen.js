3fd874f2c693067cd5ec53ba143479927987ac26
Evgeni Korotkevich <evgenikorotkevich25@gmail.com>
2025-11-14 13:17:06 +0400

    feat: create mode selection section

--- pair-em-up/src/modules/screen.js
+++ pair-em-up/src/modules/screen.js
@@ -6,7 +6,7 @@ export function createScreen() {
   const app = document.createElement("div");
   app.className = "app";
 
-  app.append(createHeader());
+  app.append(createHeader(), createModeSelection());
   document.body.append(app);
 }
 
@@ -23,4 +23,32 @@ const createHeader = () => {
   header.append(title, author);
 
   return header;
+};
+
+const createModeSelection = () => {
+  const controls = document.createElement("section");
+  controls.className = "mode-selection";
+
+  const settingBtn = document.createElement("button");
+  settingBtn.textContent = "Setting";
+  settingBtn.className = "btn setting";
+
+  const classicBtn = document.createElement("button");
+  classicBtn.textContent = "Classic";
+  classicBtn.className = "btn";
+  // classicBtn.addEventListener("click", () => startGame("classic"));
+
+  const randomBtn = document.createElement("button");
+  randomBtn.textContent = "Random";
+  randomBtn.className = "btn";
+  // randomBtn.addEventListener("click", () => startGame("random"));
+
+  const chaoticBtn = document.createElement("button");
+  chaoticBtn.textContent = "Chaotic";
+  chaoticBtn.className = "btn";
+  // chaoticBtn.addEventListener("click", () => startGame("chaotic"));
+
+  controls.append(classicBtn, randomBtn, chaoticBtn);
+
+  return controls;
 };
